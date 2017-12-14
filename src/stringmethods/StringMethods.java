39a4ceb337d663d1a44a631dadc6c6b5dbf171fc
MERC-LAB-01 <MERC-LAB-01@MERC-LAB-18>
2017-12-14 13:33:56 +0530

    first version

--- src/stringmethods/StringMethods.java
+++ src/stringmethods/StringMethods.java
@@ -7,25 +7,25 @@ package stringmethods;
 
 import javax.swing.JOptionPane;
 
-
-
 public class StringMethods {
 
     public static void main(String[] args) {
         String name = JOptionPane.showInputDialog("Enter NIC");
         int nameLength = name.length();
-        double sex;
+        double gender;
         if (nameLength == 10) {
             String vorx = name.substring(9, 10);
-            String age = name.substring(0, 2);
-            String xy = name.substring(2, 3);
-            sex = Double.parseDouble(xy);
+            //String age = name.substring(0, 2);
+            String xy = name.substring(2, 5);
+            gender = Double.parseDouble(xy); 
             if (vorx.equalsIgnoreCase("X") || vorx.equalsIgnoreCase("V")) {
                 JOptionPane.showMessageDialog(null, "NIC OK");
-                JOptionPane.showMessageDialog(null, "Your Birth year is 19" + age);
-                if (sex < 5){
-                JOptionPane.showMessageDialog(null, "You are a Male");
-                }else {JOptionPane.showMessageDialog(null,"You are a Female");}
+                JOptionPane.showMessageDialog(null, "Your Birth year is 19" + name.substring(0,2));
+                if (gender < 500) {
+                    JOptionPane.showMessageDialog(null, "You are a Male");
+                } else {
+                    JOptionPane.showMessageDialog(null, "You are a Female");
+                }
             } else {
                 JOptionPane.showMessageDialog(null, "NIC Wrong Letter");
             }
